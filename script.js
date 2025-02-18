0aa29090874a85a51cb3b7208066770bebe45701
duvalgomes <duvalfgomes.com>
2025-02-19 00:20:06 +0530

    done for day 1

---
+++ script.js
@@ -0,0 +1,20 @@
+// Function to show the contact popup
+function openPopup() {
+    document.getElementById("contact-popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById("contact-popup").style.display = "none";
+}
+
+
+// Function to show the popup (accepts popup ID)
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+}
+
+// Function to close the popup (accepts popup ID)
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
